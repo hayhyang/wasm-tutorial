54a83e06cd161ce12c0aa7a774c1726a67bf5933
rylee <rylee@moduparking.com>
2023-06-22 12:15:55 +0900

    refactor: 파일 정리

---
+++ examples/c_multiple/multiple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void multiple(int grades)
+{
+	for(int i = 1; i <=9; i++)
+	{
+		printf("%d * %d = %d \n",grades, i, grades*1);
+	}
+
+}
+
+int main()
+{
+	multiple(2);
+	return 1;
+}
